a96c37c43227eb1697e8f545812cfacbb9481289
tranquangthong <23115053122141@sv.ute.udn.vn>
2025-10-08 14:50:16 +0700

    Header and Search

---
+++ src/components/Search.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="search.svg" alt="Search" />
+        <input
+          type="text"
+          placeholder="Search through 300+ movies online"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
