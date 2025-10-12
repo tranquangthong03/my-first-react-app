e0a1f8839beff9e091a25fd77ed5a8b26fecae0e
tranquangthong <23115053122141@sv.ute.udn.vn>
2025-10-12 17:11:33 +0700

    Call API, movielist and set loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./components/Search";
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]); // State to hold the list of movies
+  const [loading, setLoading] = useState(false); // State to indicate loading status
+  const API_BASE_URL = "https://api.themoviedb.org/3";
+  const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+  const API_OPTIONS = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  };
+  const fetchMovies = async () => {
+    setLoading(true); // Set loading to true before fetching
+    setErrorMessage(""); // Clear any previous error messages
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        // if response is not ok, throw an error
+        throw new Error("Failed to fetch movies");
+      }
+      const data = await response.json();
+      console.log(data);
+      if (data.Response === "False") {
+        // Return if API returns an error
+        throw new Error("Failed to fetch movies");
+        setMovieList([]);
+        return;
+      }
+      setMovieList(data.results || []); // Update state with fetched movies
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      setErrorMessage("Failed to fetch movies. Please try again later.");
+    }
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
   return (
     <main>
       <div className="pattern"></div>
@@ -12,9 +51,12 @@ const App = () => {
             Find <span className="text-gradient">Movies</span>You'll Love
             Without the Hassle
           </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <h1 className="text-white">{searchTerm}</h1>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
       </div>
     </main>
   );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ const Search = ({ searchTerm, setSearchTerm }) => {
   return (
     <div className="search">
       <div>
-        <img src="search.svg" alt="Search" />
+        <img className="cursor-pointer" src="search.svg" alt="Search" />
         <input
           type="text"
           placeholder="Search through 300+ movies online"
